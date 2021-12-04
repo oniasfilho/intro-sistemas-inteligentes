0cda5ddff767db6659ff6453b8a75a484d8bbf76
oniasfilho <onias.filho@gmail.com>
2021-12-04 11:33:18 -0400

    Inserts second exercise and make minor changes in the first

--- main.py
+++ main.py
@@ -2,11 +2,6 @@
 # faça uma função que retorne quantas pessoas com mais de 40 anos
 # possuem peso superior à média dos pesos dessas pessoas.
 
-class pessoa:
-    def __init__(self, *peso, **idade):
-        self.idade = idade
-        self.peso = peso
-
 
 def primeiro_exercicio(idades, pesos):
     lista_pessoas = []
@@ -15,6 +10,11 @@ def primeiro_exercicio(idades, pesos):
     peso_medio = 0
     individuos_acima_40_acima_media = 0
     tamanho_lista = 0;
+
+    class pessoa:
+        def __init__(self, *peso, **idade):
+            self.idade = idade
+            self.peso = peso
 
     if len(idades) == len(pesos):
         tamanho_lista = len(idades)
@@ -43,4 +43,39 @@ def primeiro_exercicio(idades, pesos):
 
 lista_idade = [40, 45, 32, 49, 19, 60, 22, 25, 43, 49, 46]
 lista_peso = [89, 91, 150, 95, 66, 140, 87, 90, 90, 65, 170]
-print(primeiro_exercicio(lista_idade, lista_peso))
+print("Primeiro exercicio: \n Quantidade de pessoas com mais de 40 anos que possuem peso superior à média dos pesos "
+      "dessas pessoas -> ", primeiro_exercicio(lista_idade, lista_peso), "\n\n")
+
+
+# 2) Faça uma função que receba dois parâmetros: linhas e colunas.
+# A função deve desenhar uma moldura usando os caracteres '-', '|' e '*',
+# como abaixo para 1 linha e 3 colunas.
+# *---*
+# |   |
+# *---*
+def segundo_exercicio(linhas, colunas):
+    def cria_coluna(coluna):
+        line = '+'
+        for x in range(coluna):
+            line += '-'
+        line += '+'
+        print(line)
+
+    def cria_linha(linha, coluna):
+        for y in range(coluna):
+            column = '|'
+            column += ' ' * linha
+            column += '|'
+            print(column)
+
+    def desenha_moldura(linhas, colunas):
+        cria_coluna(colunas)
+        cria_linha(colunas, linhas)
+        cria_coluna(colunas)
+        print("\n\n")
+
+    desenha_moldura(linhas, colunas)
+
+
+print("Segundo exercicio:")
+segundo_exercicio(1, 3)
